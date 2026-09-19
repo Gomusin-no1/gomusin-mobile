46d9de459098391e5820369f5c738321171a324e
Gomusin-no1 <x76671100@gmail.com>
2026-09-19 05:17:54 -0700

    feat: add installable app shell

---
+++ static/sw.js
@@ -0,0 +1,5 @@
+const SHELL_CACHE='trustflow-shell-v1';
+const SHELL=['/static/offline.html','/static/images/trustflow-app-icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(SHELL_CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting()});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==SHELL_CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
+self.addEventListener('fetch',event=>{if(event.request.mode!=='navigate')return;event.respondWith(fetch(event.request).catch(()=>caches.match('/static/offline.html')))});
