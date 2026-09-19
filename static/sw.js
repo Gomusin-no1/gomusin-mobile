ef953790e72e41c56210b0b4f2fd2d0b9bc9289d
Gomusin-no1 <x76671100@gmail.com>
2026-09-19 13:05:46 -0700

    style: update TrustMap app shell

--- static/sw.js
+++ static/sw.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE='trustflow-shell-v1';
-const SHELL=['/static/offline.html','/static/images/trustflow-app-icon.svg'];
+const SHELL_CACHE='trustmap-shell-v2';
+const SHELL=['/static/offline.html','/static/images/trustmap-mark.webp'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(SHELL_CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==SHELL_CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
 self.addEventListener('fetch',event=>{if(event.request.mode!=='navigate')return;event.respondWith(fetch(event.request).catch(()=>caches.match('/static/offline.html')))});
